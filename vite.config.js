7a5b52dee30de41fc533b3f89b3df1943f76e456
rajvir singh dev <devrajvirsingh@gmail.com>
2023-12-28 20:09:51 +0530

    initial upload

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/NewsExpress-ReactJS/",
+  base: "/NewsExpress-Reactjs/",
   plugins: [react()],
 });
